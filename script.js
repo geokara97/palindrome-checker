d07a1a9daf8881c68f1244055124cb3f69c6a87b
Georgios Karagiannis <geo.karagiannis97@gmail.com>
2025-02-05 02:19:07 +0200

    second commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const button=document.getElementById('check-btn');
+
+
+
+button.addEventListener('click',function(){
+    let str=document.getElementById('text-input').value;
+    input=str.replace(/[^A-Za-z0-9]/gi, '').toLowerCase();
+    console.log(input);
+    event.preventDefault();
+    if(input===''){
+        alert('Please input value');
+    }
+    let flag=1;
+    for(let i=0;i<input.length;i++){
+        if(input[i]!=input[input.length-i-1]){
+            document.getElementById('result').textContent=`${str} is not a palindrome`;
+            flag=0;
+            break;
+        }
+        
+    }
+    if(flag===1){
+        document.getElementById('result').textContent=`${str} is a palindrome`;
+        
+    }
+})
+
